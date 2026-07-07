c1a055abbcab976c242a5681fa9d2e362086a16b
Mustafa <lasharighulammustafa934@gmail.com>
2026-07-07 14:25:57 +0500

    Add SocialMediaGUI JFrame with GUIConstants

---
+++ View/SocialMediaGUI.java
@@ -0,0 +1,10 @@
+package View;
+
+public class SocialMediaGUI extends javax.swing.JFrame{
+
+    public SocialMediaGUI(){
+        super("Social Media GUI");
+        getContentPane().setBackground();
+        
+    }
+}
